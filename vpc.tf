47a48773eed829b90d3577904cd41aec93a51bef
Pimz <pmpz@home.com>
2021-05-18 10:40:01 +0000

    add new vpc module
    Signed-off-by: Pimz <pmpz@home.com>

---
+++ vpc.tf
@@ -0,0 +1,15 @@
+module "network" {
+  source  = "app.terraform.io/vlica-training/network/google"
+  version = "2.5.0"
+  # insert required variables here
+}
+
+network_name = "guarav-network"
+project_id = var.project
+subnets = [
+    {
+        subnet_name = "guarav-subnet"
+        subnet_ip = "10.100.10.0/24"
+        subnet_region = var.region
+    }
+]
